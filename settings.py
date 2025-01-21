574003be210f52e518907a3f673f1e62f93458a9
ryzov <ryzovgrisa8@gmail.com>
2025-01-22 00:02:52 +0300

    ы

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 TOKEN = '8149697457:AAGQY73ctaCxCx-5IYwWWfgawr7JxxoQERE'
 ADMINS = [2075010057, 127716206, 629401483] # TODO: ❗❗❗АДМИН ДОЛЖЕН НАХОДИТЬСЯ НА ПЕРВОМ МЕСТЕ
-BASED_FOLDER_ID = "1XuYfGz994fr-_wljqpTfHOb1RJqJ1zAc"
-#1Fx09qIryiKi3Nj1CtWMqomh14Xc_BfX1
+BASED_FOLDER_ID = "1Fx09qIryiKi3Nj1CtWMqomh14Xc_BfX1"
+#1Fx09qIryiKi3Nj1CtWMqomh14Xc_BfX1 https://drive.google.com/file/d/1TFzBNyzDTZD1IvNU3_sZwnLCqGRgHYAs/view?usp=sharing
 ID_MISTAKES = -4545307339
 # todo: узнать, будет ли кто-то ещё в админах, а не только дядя, + добавить айди дяди
 # todo: поставить токен бота дяди, не нашего
